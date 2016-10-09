69b0cebc52f3599a05bf05e15332d245f0ffa3b7
arya45 <audrey.dogbeh@gmail.com>
2016-10-09 16:19:11 +0100

    Add rest of elements to getWeather function

--- js/script.js
+++ js/script.js
@@ -6,16 +6,17 @@ $(document).ready(function(){
 		$('main').css("height",$('body').height()-$('footer').innerHeight());
 	});
     
-    $("button").click(getWeather);
+    $("#searchBar button").click(getWeather);
         
     function getWeather(){
         var userLocation = $('#locationInput').val();
-        console.log(userLocation);
-        var userUrl = "http://api.openweathermap.org/data/2.5/weather?q="+userLocation+"&APPID=9ff0ea3fc4229de7fc7df29f32c9427b";
+        var userUrl = "http://api.openweathermap.org/data/2.5/weather?q="+userLocation+"&units=metric&APPID=9ff0ea3fc4229de7fc7df29f32c9427b";
         $.getJSON(userUrl, function(result){
             $("#location").text(result.name+', '+result.sys.country);
             $("#coord").text("Lon: "+result.coord.lon+" | Lat: "+result.coord.lat);
-            $("#dateTime").append(result.dt);
+            $("#mainTemp").text(Math.round(result.main.temp)+"°C");
+            $("#windSpeed").text(result.wind.speed+"m/s");
+            $("#dateTime").text("Last updated: "+result.dt);
         });
     };
 })
