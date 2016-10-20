a14eae77792c6a1da829d272ac223e3366e94946
arya45 <audrey.dogbeh@gmail.com>
2016-10-20 20:04:08 +0100

    Make unit switch functional

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
 		$('html').css('height',$(window).height());
 		$('main').css("height",$('body').height()-$('footer').innerHeight());
 	});
-    
+
     $("#searchBar button").click(getWeather);
     /*and to handle Enter key press - some browsers use 'keyCode', some use 'which' - 13 is keyCode for Enter */
     $("#locationInput").keydown(function(event){
@@ -13,16 +13,32 @@ $(document).ready(function(){
             getWeather();
         }
     });
-        
+     $('#unitSwitch').click(function(){
+         liveSwitch();
+     });
+    
+    var userLocation, userURL, d, unit, sym;
     function getWeather(){
-        var userLocation = $('#locationInput').val();
-        var userUrl = "http://api.openweathermap.org/data/2.5/weather?q="+userLocation+"&units=metric&APPID=9ff0ea3fc4229de7fc7df29f32c9427b";
+        unit = $('input[type="checkbox"]').prop('checked')? 'imperial' : 'metric';
+        sym = (unit=='metric')? '°C' : '°F';
+        userLocation = $('#locationInput').val();
+        userUrl = "http://api.openweathermap.org/data/2.5/weather?q="+userLocation+"&units="+unit+"&APPID=9ff0ea3fc4229de7fc7df29f32c9427b";
         $.getJSON(userUrl, function(result){
             $("#location").text(result.name+', '+result.sys.country);
             $("#coord").text("Lon: "+result.coord.lon+" | Lat: "+result.coord.lat);
-            $("#mainTemp").html(Math.round(result.main.temp)+"<span class='greyText'>°C</span>");
-            $("#windSpeed").html(result.wind.speed+"<span class='greyText' style='font-size:2.5rem'>m/s</span>");
-            $("#dateTime").text("Last updated: "+result.dt);
-        });
-    };
+            $("#mainTemp").html(Math.round(result.main.temp)+"<span class='greyText'>"+sym+"</span>");
+            $("#windSpeed").html(Math.round(result.wind.speed)+"<span class='greyText' style='font-size:2.5rem'>m/s</span>");
+            d = new Date();
+            $("#dateTime").text("Last updated: "+d.toLocaleTimeString());
+        })
+    }
+
+    function liveSwitch(){
+        unit = $('input[type="checkbox"]').prop('checked')? 'imperial' : 'metric';
+        sym = (unit=='metric')? '°C' : '°F';
+        userUrl = "http://api.openweathermap.org/data/2.5/weather?q="+userLocation+"&units="+unit+"&APPID=9ff0ea3fc4229de7fc7df29f32c9427b";
+        $.getJSON(userUrl, function(result){
+            $("#mainTemp").html(Math.round(result.main.temp)+"<span class='greyText'>"+sym+"</span>");
+        })
+    }
 })
