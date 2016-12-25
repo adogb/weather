de576f9aefe8285a1145c31cda145fa84a118158
arya45 <audrey.dogbeh@gmail.com>
2016-12-25 01:17:40 +0100

    Day/night icon fix, imperial wind speed

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 
     /*Initialise variables*/
-    var userLocation, userURL, d, unit, sym, temp, tempMetric, tempFarenheit, sunset, sunrise;
+    var userLocation, userURL, d, updateTime, unit, symWind, symTemp, temp, wind, deg, tempMetric, tempFarenheit, windMetric, windFarenheit, sunset, sunrise, icon;
     
     /* Initialise with a city to avoid blank page */
     getWeather('Saint Tropez');
@@ -27,32 +27,52 @@ $(document).ready(function(){
 
     /*Functions declarations*/
     function getWeather(location){
-        d = new Date();
-        $("#dateTime").text("Last updated: "+d.toLocaleTimeString());
-        unit = $('input[type="checkbox"]').prop('checked')? 'imperial' : 'metric';
-        sym = (unit=='metric')? '°C' : '°F';
         userUrl = "http://api.openweathermap.org/data/2.5/weather?q="+location+"&APPID=9ff0ea3fc4229de7fc7df29f32c9427b";
         $.getJSON(userUrl, function(result){
             $("#location").text(result.name+', '+result.sys.country);
             $("#coord").text("Lon: "+result.coord.lon+" | Lat: "+result.coord.lat);
+            $("#desc").html(result.weather[0].description);
+            
             tempMetric = result.main.temp - 273.15;
             tempFarenheit = result.main.temp * 9/5 - 459.67;
-            temp = unit=='metric'? tempMetric : tempFarenheit;
-            $("#mainTemp").html(Math.round(temp)+"<span class='greyText'>"+sym+"</span>");
-            $("#windSpeed").html('<i class="wi wi-wind towards-'+Math.round(result.wind.deg)+'-deg"></i>'+Math.round(result.wind.speed)+"<span class='greyText'>m/s</span>");
-            /*sunrise = result.sys.sunrise;
+            windMetric = result.wind.speed;
+            windFarenheit = windMetric*2.24;
+            deg = result.wind.deg;
+            liveSwitch();
+            
+            updateTime = result.dt;
+            d = new Date(updateTime*1000); //multiply by 1000 because result.dt is in seconds, while Date() works with milliseconds
+            $("#dateTime").text("Weather data last acquired: "+d.toTimeString());
+            sunrise = result.sys.sunrise;
             sunset = result.sys.sunset;
-            console.log(result.dt, sunrise, sunset);*/
-            console.log(result.weather[0].id+" "+result.weather[0].icon);
-            $('#weatherIcon i').removeClass().addClass('wi wi-owm-'+result.weather[0].id);
-            $("#desc").html(result.weather[0].description);
+            /* adjusting sunrise/sunset days to take into account timezones: sunrise/sunset data given by openweathermap are related
+            to the current day in UTC timezone, so need adjusting by +/- 1 day i.e. 84600 seconds
+            if a timezone has not yet reached / has already passed that day */
+            //console.log(result.weather[0].icon, updateTime, sunrise, sunset);
+            if (updateTime<sunrise && updateTime<sunset-84600){
+                sunrise=sunrise-84600;
+                sunset=sunset-84600;
+            } else if (updateTime>sunset && updateTime>sunrise+84600){
+                sunrise=sunrise+84600;
+                sunset=sunset+84600;
+            }
+            /*normally compare time to sunrise / sunset to define day or night weather icon */
+            if (updateTime<sunrise || updateTime>sunset){
+                icon='night';
+            } else {
+                icon='day';
+            }
+            $('#weatherIcon i').removeClass().addClass('wi wi-owm-'+icon+'-'+result.weather[0].id);
         });
     }
 
     function liveSwitch(){
         unit = $('input[type="checkbox"]').prop('checked')? 'imperial' : 'metric';
-        sym = (unit=='metric')? '°C' : '°F';
+        symTemp = (unit=='metric')? '°C' : '°F';
+        symWind = (unit=='metric')? 'm/s' : 'mph';
         temp = unit=='metric'? tempMetric : tempFarenheit;
-        $("#mainTemp").html(Math.round(temp)+"<span class='greyText'>"+sym+"</span>");
+        wind = unit=='metric'? windMetric : windFarenheit;
+        $("#mainTemp").html(Math.round(temp)+"<span class='greyText'>"+symTemp+"</span>");
+        $("#windSpeed").html('<i class="wi wi-wind towards-'+Math.round(deg)+'-deg"></i>'+Math.round(wind)+"<span class='greyText'>"+symWind+"</span>");
     }
 })
